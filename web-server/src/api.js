5da3bff93a86fca14759741d4e4cf8607d90af71
wj456-psu <6310110456@psu.ac.th>
2023-03-18 03:27:33 +0700

    add goods table in db

--- web-server/src/api.js
+++ web-server/src/api.js
@@ -41,6 +41,72 @@ router.get("/user/:name", (req, res) => {
   res.json({ "message": "success", "data": user });
 });
 
+router.get("/goods", (req, res) => {
+  const sql = "SELECT * FROM goods";
+  const params = [];
+  const goods = db.prepare(sql).all(params);
+  res.json({ "message": "success", "data": goods });
+});
+
+router.get("/good/:id", (req, res) => {
+  const sql = "SELECT * FROM goods WHERE id = ?";
+  const params = [req.params.id];
+  const good = db.prepare(sql).get(params);
+  if (!good) {
+    res.status(404).json({ "error": "No good found with the specified id" });
+    return;
+  }
+  res.json({ "message": "success", "data": good });
+});
+
+router.post("/device", (req, res) => {
+  if (!req.body.id) {
+    res.status(400).json({ "error": "No id specified" });
+    return;
+  }
+  const sql = "INSERT INTO devices (id) VALUES (?)";
+  const params = [req.body.id];
+  try {
+    db.prepare(sql).run(params);
+    res.json({
+      "message": "success",
+      "data": data,
+    });
+  } catch (err) {
+    res.status(400).json({ "error": err.message });
+    return;
+  }
+});
+
+router.post("/device/:id", (req, res) => {
+  if (!req.body.username) {
+    res.status(400).json({ "error": "Missing required parameter" });
+    return;
+  }
+  const sql =
+    `UPDATE devices SET
+      used = TRUE,
+      username = ?
+      WHERE id = ? `;
+  const data = {
+    name: req.body.username,
+    id: req.params.id
+  }
+  const params = [data.name, data.id];
+  try {
+    db.prepare(sql).run(params);
+    res.json({
+      "message": "success",
+      "data": { "device_id": data.id, "used_by": data.name }
+    });
+  } catch (err) {
+    if (err.message === "FOREIGN KEY constraint failed") {
+      res.status(400).json({ "error": "No user: " + data.name });
+      return;
+    }
+  }
+});
+
 router.post("/user", (req, res) => {
   const errors = [];
   if (!req.body.username) {
@@ -71,73 +137,42 @@ router.post("/user", (req, res) => {
   }
 });
 
-router.put("/user/:name", (req, res) => {
+router.post("/good", (req, res) => {
+  const errors = [];
+  if (!req.body.id) {
+    errors.push("No id specified");
+  }
+  if (!req.body.name) {
+    errors.push("No name specified");
+  }
+  if (!req.body.price) {
+    errors.push("No price specified");
+  }
+  if (!req.body.img) {
+    errors.push("No img specified");
+  }
+  if (errors.length) {
+    res.status(400).json({ "error": errors.join(", ") });
+    return;
+  }
   const data = {
-    password: req.body.password,
-    balance: req.body.balance
+    id: req.body.id,
+    name: req.body.name,
+    price: req.body.price,
+    img: req.body.img,
+    left: req.body.left
   }
-  console.log(data)
-  const isEmpty = Object.values(data).every(x => x === undefined);
-  if (isEmpty) {
-    res.status(400).json({ "error": "No key specified" });
-    return;
-  }
-  if (data.balance) {
-    data.balance = parseFloat(data.balance);
-  }
-  const sql = `UPDATE users SET
-      password = COALESCE(?, password),
-      balance = COALESCE(?, balance)
-      WHERE username = ?`;
-  const params = [data.password, data.balance, req.params.name];
-  const info = db.prepare(sql).run(params);
-  res.json({
-    "message": "success",
-    "data": data,
-    "changes": info.changes
-  });
-});
-
-router.delete("/user/:name", (req, res) => {
-  const sql = "DELETE FROM users WHERE username = ?";
-  const params = [req.params.name];
-  const info = db.prepare(sql).run(params);
-  if (info.changes === 0) {
-    res.status(404).json({ "error": "No user found with the specified username" });
-    return;
-  }
-  res.json({
-    "message": "deleted",
-    "changes": info.changes
-  });
-});
-
-router.post("/device/:id", (req, res) => {
-  if (!req.body.username) {
-    res.status(400).json({ "error": "Missing required parameter" });
-    return;
-  }
-  const sql =
-    `UPDATE devices SET
-      used = TRUE,
-      username = ?
-      WHERE id = ? `;
-  const data = {
-    name: req.body.username,
-    id: req.params.id
-  }
-  const params = [data.name, data.id];
+  const sql = "INSERT INTO goods (id, name, price, img, left) VALUES (?, ?, ?, ?, ?)";
+  const params = [data.id, data.name, data.price, data.img, data.left];
   try {
     db.prepare(sql).run(params);
     res.json({
       "message": "success",
-      "data": { "device_id": data.id, "used_by": data.name }
+      "data": data,
     });
   } catch (err) {
-    if (err.message === "FOREIGN KEY constraint failed") {
-      res.status(400).json({ "error": "No user: " + data.name });
-      return;
-    }
+    res.status(400).json({ "error": err.message });
+    return;
   }
 });
 
@@ -191,6 +226,109 @@ router.put("/device/:id", (req, res) => {
       "changes": info.changes
     });
   }
+});
+
+router.put("/user/:name", (req, res) => {
+  const data = {
+    password: req.body.password,
+    balance: req.body.balance
+  }
+  console.log(data)
+  const isEmpty = Object.values(data).every(x => x === undefined);
+  if (isEmpty) {
+    res.status(400).json({ "error": "No key specified" });
+    return;
+  }
+  if (data.balance) {
+    data.balance = parseFloat(data.balance);
+  }
+  const sql = `UPDATE users SET
+      password = COALESCE(?, password),
+      balance = COALESCE(?, balance)
+      WHERE username = ?`;
+  const params = [data.password, data.balance, req.params.name];
+  const info = db.prepare(sql).run(params);
+  res.json({
+    "message": "success",
+    "data": data,
+    "changes": info.changes
+  });
+});
+
+router.put("/good/:id", (req, res) => {
+  const data = {
+    name: req.body.name,
+    price: req.body.price,
+    img: req.body.img,
+    left: req.body.left
+  }
+  console.log(data);
+  const isEmpty = Object.values(data).every(x => x === undefined);
+  if (isEmpty) {
+    res.status(400).json({ "error": "No key specified" });
+    return;
+  }
+  if (data.price) {
+    data.price = parseFloat(data.price);
+  }
+  if (data.left) {
+    data.left = parseInt(data.left);
+  }
+  const sql = `UPDATE goods SET
+      name = COALESCE(?, name),
+      price = COALESCE(?, price),
+      img = COALESCE(?, img),
+      left = COALESCE(?, left)
+      WHERE id = ?`;
+  const params = [data.name, data.price, data.img, data.left, req.params.id];
+  const info = db.prepare(sql).run(params);
+  res.json({
+    "message": "success",
+    "data": data,
+    "changes": info.changes
+  });
+});
+
+router.delete("/device/:id", (req, res) => {
+  const sql = "DELETE FROM devices WHERE id = ?";
+  const params = [req.params.id];
+  const info = db.prepare(sql).run(params);
+  if (info.changes === 0) {
+    res.status(404).json({ "error": "No device found with the specified id" });
+    return;
+  }
+  res.json({
+    "message": "deleted",
+    "changes": info.changes
+  });
+});
+
+router.delete("/user/:name", (req, res) => {
+  const sql = "DELETE FROM users WHERE username = ?";
+  const params = [req.params.name];
+  const info = db.prepare(sql).run(params);
+  if (info.changes === 0) {
+    res.status(404).json({ "error": "No user found with the specified username" });
+    return;
+  }
+  res.json({
+    "message": "deleted",
+    "changes": info.changes
+  });
+});
+
+router.delete("/good/:id", (req, res) => {
+  const sql = "DELETE FROM goods WHERE id = ?";
+  const params = [req.params.id];
+  const info = db.prepare(sql).run(params);
+  if (info.changes === 0) {
+    res.status(404).json({ "error": "No good found with the specified id" });
+    return;
+  }
+  res.json({
+    "message": "deleted",
+    "changes": info.changes
+  });
 });
 
 module.exports = router
--- web-server/src/db.js
+++ web-server/src/db.js
@@ -18,7 +18,6 @@ try {
 try {
     db.prepare(`CREATE TABLE devices (
         id INTEGER,
-        password TEXT,
         used INTEGER DEFAULT FALSE,
         username TEXT,
         time_remained INTEGER DEFAULT 0,
@@ -26,19 +25,22 @@ try {
         FOREIGN KEY(username) REFERENCES users(username)
     )`).run();
 } catch (err) { }
-const insertDevice = db.prepare(`INSERT INTO devices (id, password) VALUES (?, ?)`);
+try {
+    db.prepare(`CREATE TABLE goods (
+        id INTEGER,
+        name TEXT,
+        price REAL,
+        img TEXT,
+        left INTEGER DEFAULT 0,
+        PRIMARY KEY(id)
+    )`).run();
+} catch (err) { }
+const insertDevice = db.prepare(`INSERT INTO devices (id) VALUES (?)`);
 const insertDevices = db.transaction((devices) => {
     for (const device of devices) insertDevice.run(device);
 });
 try {
-    insertDevices([
-        [1, "aaaa"],
-        [2, "bbbb"],
-        [3, "cccc"],
-        [4, "dddd"],
-        [5, "eeee"],
-        [6, "ffff"],
-    ]);
+    insertDevices([1, 2, 3, 4, 5, 6,]);
 } catch (err) { }
 const insertAdmin = db.prepare(`INSERT INTO users (username, password, isAdmin) VALUES (?, ?, ?)`);
 const insertAdmins = db.transaction((admins) => {
@@ -47,6 +49,17 @@ const insertAdmins = db.transaction((admins) => {
 try {
     insertAdmins([
         ["Uan", "admin", 1],
+    ]);
+} catch (err) { }
+const insertGood = db.prepare(`INSERT INTO goods (id, name, price, img, left) VALUES (?, ?, ?, ?, ?)`);
+const insertGoods = db.transaction((goods) => {
+    for (const good of goods) insertGood.run(good);
+});
+try {
+    insertGoods([
+        [1, "Snack", 30, "https://cdn-icons-png.flaticon.com/512/859/859293.png", 12],
+        [2, "Cookies", 10, "https://cdn-icons-png.flaticon.com/512/526/526331.png", 12],
+        [3, "Soda", 20, "https://cdn-icons-png.flaticon.com/512/3076/3076028.png", 12],
     ]);
 } catch (err) { }
 
