66af01821b4d6b131fef6dab8d0440c8c0b65a15
punchhero159 <punchhero159@hotmail.com>
2023-03-18 18:19:42 +0700

    Notfound_page_update

--- user-client/src/containers/NotFound.js
+++ user-client/src/containers/NotFound.js
@@ -4,7 +4,12 @@ import "./NotFound.css";
 export default function NotFound() {
   return (
     <div className="NotFound text-center">
-      <h3>Sorry, page not found!</h3>
+      <h1 className="mb-4">Oops!</h1>
+      <h3 className="mb-4">We couldn't find what you were looking for.</h3>
+      <p className="mb-5">But don't worry, we've got you covered.</p>
+      <a href="/" className="btn btn-primary">
+        Back Home
+      </a>
     </div>
   );
 }
